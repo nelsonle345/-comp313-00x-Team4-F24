1c43c487c58fa57f0ac55c7254706d413068858a
HobbyistProgrammer <benton.le@hotmail.com>
2024-10-09 19:04:55 -0400

    Created and Linked Firebase Database
    Created a simple view page for users and user registration

--- app/src/main/java/com/comp313sec401/group4/shovelhero/MainActivity.kt
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comp313sec401.group4.shovelhero
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +12,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.RecyclerView
 import com.comp313sec401.group4.shovelhero.ui.theme.ShovelHeroTheme
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.FirebaseDatabase
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var mainAdapter: MainAdapter
+    private lateinit var floatingActionButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ShovelHeroTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.userView)
+        recyclerView.layoutManager = CustomLinearLayoutManager(this)
+
+        val options = FirebaseRecyclerOptions.Builder<User>()
+            .setQuery(FirebaseDatabase.getInstance().getReference("users"), User::class.java)
+            .build()
+
+        mainAdapter = MainAdapter(options)
+        recyclerView.adapter = mainAdapter
+
+        floatingActionButton = findViewById(R.id.floatingAddUserButton)
+        floatingActionButton.setOnClickListener {
+            startActivity(Intent(applicationContext, CreateUserActivity::class.java))
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onStart() {
+        super.onStart()
+        mainAdapter.startListening()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShovelHeroTheme {
-        Greeting("Android")
+    override fun onStop() {
+        super.onStop()
+        mainAdapter.stopListening()
     }
 }
