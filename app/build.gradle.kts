bfa05eb40c197319a86428f305ab71bc629691d6
HobbyistProgrammer <benton.le@hotmail.com>
2024-10-09 18:30:58 -0400

    Synced Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.comp313sec401.group4.shovelhero"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.comp313sec401.group4.shovelhero"
