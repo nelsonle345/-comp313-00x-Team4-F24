344015bf981f1e744591f335f56c8209c3fc9cda
fatimah <fatimahbintiyasin5@gmail.com>
2024-10-09 19:55:30 -0400

    landing page and 3 Users Profile view pages

---
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/MainActivity.java
@@ -0,0 +1,21 @@
+package com.comp313sec401.group4.shovelhero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.etUsername);
+        passwordEditText = findViewById(R.id.etPassword);
+    }
+
+
+}
