2feec3297365f7fd666c67f2465ec33e0342543d
HobbyistProgrammer <benton.le@hotmail.com>
2024-10-12 18:02:39 -0400

    Gradle Fixes

--- app/src/main/java/com/comp313sec401/group4/shovelhero/TempMainActivity.kt
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/TempMainActivity.kt
@@ -2,23 +2,13 @@ package com.comp313sec401.group4.shovelhero
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.RecyclerView
-import com.comp313sec401.group4.shovelhero.ui.theme.ShovelHeroTheme
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.FirebaseDatabase
 
-class MainActivity : AppCompatActivity() {
+class TempMainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var mainAdapter: MainAdapter
@@ -28,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.userView)
+        // recyclerView = findViewById(R.id.userView)
         recyclerView.layoutManager = CustomLinearLayoutManager(this)
 
         val options = FirebaseRecyclerOptions.Builder<User>()
@@ -38,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         mainAdapter = MainAdapter(options)
         recyclerView.adapter = mainAdapter
 
-        floatingActionButton = findViewById(R.id.floatingAddUserButton)
+        // floatingActionButton = findViewById(R.id.floatingAddUserButton)
         floatingActionButton.setOnClickListener {
             startActivity(Intent(applicationContext, CreateUserActivity::class.java))
         }
