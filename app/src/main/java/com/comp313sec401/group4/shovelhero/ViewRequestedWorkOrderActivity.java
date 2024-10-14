c8f5d2c900d57c293e1431f4a07a8d65e593030d
Nelson Le <nelsonle345@gmail.com>
2024-10-14 12:55:57 -0400

    Updated the UI and layout for "Accept Open Work Order" and "View Requested Work Order"

--- app/src/main/java/com/comp313sec401/group4/shovelhero/ViewRequestedWorkOrderActivity.java
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/ViewRequestedWorkOrderActivity.java
@@ -1,14 +1,13 @@
 package com.comp313sec401.group4.shovelhero;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Accept_Or_Decline_Work extends AppCompatActivity {
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import androidx.appcompat.app.AppCompatActivity;
+public class ViewRequestedWorkOrderActivity extends AppCompatActivity {
 
     private Button acceptButton, declineButton;
     private EditText commentsEditText;
@@ -17,7 +16,7 @@ public class Accept_Or_Decline_Work extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accept_open_work);
+        setContentView(R.layout.activity_view_requested_work_order);
 
         acceptButton = findViewById(R.id.acceptButton);
         declineButton = findViewById(R.id.declineButton);
@@ -35,14 +34,14 @@ public class Accept_Or_Decline_Work extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String comments = commentsEditText.getText().toString().trim();
-                Toast.makeText(Accept_Or_Decline_Work.this, "Work Order Accepted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ViewRequestedWorkOrderActivity.this, "Work Order Accepted", Toast.LENGTH_SHORT).show();
             }
         });
 
         declineButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Accept_Or_Decline_Work.this, "Work Order Declined", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ViewRequestedWorkOrderActivity.this, "Work Order Declined", Toast.LENGTH_SHORT).show();
             }
         });
     }
