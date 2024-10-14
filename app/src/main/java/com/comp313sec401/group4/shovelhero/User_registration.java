fa9fa2866a6423a8eb361fa38b8deb82244e22ea
fatimah <fatimahbintiyasin5@gmail.com>
2024-10-14 12:37:46 -0400

    Payment added

--- app/src/main/java/com/comp313sec401/group4/shovelhero/User_registration.java
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/User_registration.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,7 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class User_registration extends AppCompatActivity {
     private String userId;
@@ -53,10 +55,6 @@ public class User_registration extends AppCompatActivity {
         uploadIdImage = findViewById(R.id.imgUploadIdCard);
         createAccountButton = findViewById(R.id.btnCreateAccount);
 
-        // Set visibility of ID card button and add ID text to hidden
-        uploadIdImage.setVisibility(View.GONE);
-        uploadIdCardTextView.setVisibility(View.GONE);
-
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,23 +66,23 @@ public class User_registration extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String phone = phoneEditText.getText().toString();
 
-                if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()){
+                if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phone.isEmpty()) {
                     Toast.makeText(User_registration.this, "Please fill out all the fields", Toast.LENGTH_SHORT).show();
                 }
-                if(username.length() <= 3 || username.length() >= 10){
+                if (username.length() <= 3 || username.length() >= 10) {
                     Toast.makeText(User_registration.this, "Please enter valid username", Toast.LENGTH_SHORT).show();
                 }
-                if(!(password.length() >= 8 && password.matches(".*\\d.*") && password.matches(".*[a-zA-Z].*"))){
+                if (!(password.length() >= 8 && password.matches(".*\\d.*") && password.matches(".*[a-zA-Z].*"))) {
                     Toast.makeText(User_registration.this, "Password must contain at least 8 characters and one letter and one digit", Toast.LENGTH_SHORT).show();
                 }
                 if (!password.equals(confirmPassword)) {
                     Toast.makeText(User_registration.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!(android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())){
+                if (!(android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
                     Toast.makeText(User_registration.this, "Please enter valid email", Toast.LENGTH_SHORT).show();
                 }
-                if(!(android.util.Patterns.PHONE.matcher(phone).matches())){
+                if (!(android.util.Patterns.PHONE.matcher(phone).matches())) {
                     Toast.makeText(User_registration.this, "Please enter valid phone number", Toast.LENGTH_SHORT).show();
                 }
             }
--- app/src/main/java/com/comp313sec401/group4/shovelhero/YouthShovellerProfile.java
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/YouthShovellerProfile.java
@@ -2,13 +2,43 @@ package com.comp313sec401.group4.shovelhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class YouthShovellerProfile extends AppCompatActivity {
+
+    Button btnViewJobs;
+    Button btnManagePaymentInfo;
+    Button btnManageProfileInfo;
+    Button btnAddAddress;
+    Button btnEditPassword;
+    Button btnViewRatings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youth_shoveller_profile);
+
+        btnViewJobs = findViewById(R.id.btnViewJobs);
+        btnManagePaymentInfo = findViewById(R.id.btnManagePaymentInfo);
+        btnManageProfileInfo = findViewById(R.id.btnManageProfileInfo);
+        btnEditPassword = findViewById(R.id.btnEditPassword);
+        btnViewRatings = findViewById(R.id.btnViewRatings);
+
+        //MANAGE PAYMENT BUTTON
+        btnManagePaymentInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(YouthShovellerProfile.this, "Temp msg: Manage Payment activity under construction", Toast.LENGTH_SHORT).show();
+
+                Intent intentManageYouthPayment = new Intent(YouthShovellerProfile.this, Manage_Payment.class);
+                //String youthId = user.getUserId();
+                //intentManageYouthPayment.putExtra("USER_ID", youthId);
+                startActivity(intentManageYouthPayment);
+            }
+        });
     }
 }
