d9f7a6078d5f397d82319d9fcaf7dd14c3bcc2cc
Nelson Le <nelsonle345@gmail.com>
2024-10-15 23:06:41 -0400

    Added the upload image function to Young Shoveller Profile

--- app/src/main/java/com/comp313sec401/group4/shovelhero/YouthShovellerProfile.java
+++ app/src/main/java/com/comp313sec401/group4/shovelhero/YouthShovellerProfile.java
@@ -3,12 +3,51 @@ package com.comp313sec401.group4.shovelhero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageButton;
+import java.io.IOException;
+import androidx.annotation.Nullable;
 
 public class YouthShovellerProfile extends AppCompatActivity {
-
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageButton imgProfilePicture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youth_shoveller_profile);
+
+        imgProfilePicture = findViewById(R.id.imgProfilePicture);
+
+        if (imgProfilePicture != null) {
+            imgProfilePicture.setOnClickListener(v -> openImageChooser());
+        } else {
+            Log.e("YouthShovellerProfile", "ImageButton imgProfilePicture is null.");
+        }
+    }
+
+    private void openImageChooser() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imgProfilePicture.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
