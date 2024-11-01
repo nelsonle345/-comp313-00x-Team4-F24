c1bcea676fdd652c7ae95beae40db09aef0e998f
HobbyistProgrammer <benton.le@hotmail.com>
2024-11-01 19:59:35 -0400

    Gradle Issue Resolved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.comp313sec401.group4.shovelhero"
         minSdk = 24
-        targetSdk = 34
+        //noinspection OldTargetApi
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
